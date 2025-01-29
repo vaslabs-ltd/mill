f08c12514ef41cb819de8f828767714fd0971d6f
irodotos <irodotos@vaslabs.io>
2025-01-29 12:12:50 +0200

    kotlin publish aar init

---
+++ example/android/kotlinlib/3-sum-lib/sumLib/src/test/kotlin/com/sumlib/SumlibUnitTest.kt
@@ -0,0 +1,17 @@
+package com.sumlib
+
+import org.junit.Assert.*
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class SumlibUnitTest {
+
+    @Test
+    fun kotlin_dir_text_size_is_correct() {
+        assertEquals(2, Main.sum(1, 1))
+    }
+}
