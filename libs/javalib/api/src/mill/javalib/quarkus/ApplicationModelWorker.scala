1864a029c8ab02c00d9e6320d39991c80bbef1fa
Vasilis Nicolaou <vasilis.nicolaou@vaslabs.io>
2026-02-12 03:59:09 +0200

    Quarkus - getting started (#6771)
    ## Getting Started with Quarkus
    
    Quarkus seems to have a different flow from the Apps we integrated so
    far. A simplified flow of it is:
    
    1. Gather the modules and dependencies and group them into runtime,
    extensions, deployment and compile time
    2. Create a Quarkus application model out of step 1
    3. Bootstrap the Quarkus App using the application model (which itself
    classloads the whole dependency tree from step 1 but using information
    from the model in step 2). Bootstrap has multiple steps according to the
    application model specification (e.g. jvm, optimised jvm and native)
    
    ## What this PR provides
    
    Because it seems the work to fully (or at least satisfactorily) support
    Quarkus is going to be a lot, this PR just provides the ability to
    create a runnable quarkus app from a single module.
    
    It does all 3 steps from above, but is missing support for multi-module
    and thus tests are not supported yet, but I'll follow up with more PRs -
    just getting a base in with some test coverage.
    
    I'm also not going to add Quarkus docs yet in mill because this is
    incomplete and highly experimental, but I will do so once I have the
    test module support in.
    
    ## How this PR was developed
    
    In general I started with creating a quarkus project (after I realised
    this was not a traditional setup) with gradle using the getting [started
    guide](https://quarkus.io/guides/getting-started) and running
    
    ```bash
    quarkus create app org.acme:getting-started \
        --extension='rest' \
        --gradle
    ```
    
    Then ran the gradle build process and started to look into the
    artifacts.
    
    <img width="288" height="326" alt="image"
    src="https://github.com/user-attachments/assets/24cfc08a-4d34-4eff-b67b-44804233351e"
    />
    
    From then, it was clear we could not manually create those structures
    and archives. So I followed the [Gradle
    implementation](https://github.com/quarkusio/quarkus/blob/main/devtools/gradle/gradle-model/src/main/java/io/quarkus/gradle/tooling/GradleApplicationModelBuilder.java)
    (it was much simpler to follow than maven's even with the information
    they expose as black boxes) . I focused on getting the bare minimum
    first so we have a runnable app.
    
    The major difference I kept from the gradle adaptation is the dependency
    resolution, I kept Mill's and giving resolved dependencies to the
    quarkus bootstrap
    
    ## Future PRs
    
    I hope this PR is enough as a first step and I can follow with more PRs
    to support:
    
    - dev and prod mode
    - testing
    - native
    - whatever else we find along

---
+++ libs/javalib/api/src/mill/javalib/quarkus/ApplicationModelWorker.scala
@@ -0,0 +1,60 @@
+package mill.javalib.quarkus
+
+import mill.api.daemon.experimental
+
+@experimental
+trait ApplicationModelWorker extends AutoCloseable {
+  def quarkusBootstrapApplication(
+      applicationModelFile: os.Path,
+      destRunJar: os.Path,
+      jar: os.Path
+  ): os.Path
+  def quarkusGenerateApplicationModel(
+      appModel: ApplicationModelWorker.AppModel,
+      destination: os.Path
+  ): os.Path
+
+  def quarkusDeploymentDependencies(runtimeDeps: Seq[ApplicationModelWorker.Dependency])
+      : Seq[ApplicationModelWorker.Dependency]
+}
+
+object ApplicationModelWorker {
+
+  /**
+   * This app model has the necessary
+   * elements to build the Quarkus Application Model. This data
+   * class is used to populate the quarkus `ApplicationModelBuilder` and `PlatformInfo`
+   * which are serialized for the QuarkusBootstrap to be able to create the Quarkus build artifacts.
+   *
+   * The effort for Quarkus support is ongoing.
+   *
+   * For details on the requirements see [[https://github.com/quarkusio/quarkus/tree/main/independent-projects/bootstrap/app-model/src/main/java/io/quarkus/bootstrap/model]]
+   */
+  case class AppModel(
+      projectRoot: os.Path,
+      buildDir: os.Path,
+      buildFile: os.Path,
+      quarkusVersion: String,
+      groupId: String,
+      artifactId: String,
+      version: String,
+      sourcesDir: os.Path,
+      resourcesDir: os.Path,
+      compiledPath: os.Path,
+      compiledResources: os.Path,
+      boms: Seq[String],
+      dependencies: Seq[Dependency],
+      nativeImage: String
+  )
+
+  case class Dependency(
+      groupId: String,
+      artifactId: String,
+      version: String,
+      resolvedPath: os.Path,
+      isRuntime: Boolean,
+      isDeployment: Boolean,
+      isTopLevelArtifact: Boolean,
+      hasExtension: Boolean
+  )
+}
---
+++ libs/javalib/src/mill/javalib/quarkus/QuarkusModule.scala
@@ -0,0 +1,310 @@
+package mill.javalib.quarkus
+
+import coursier.core.VariantSelector.ConfigurationBased
+import mill.api.PathRef
+import mill.{T, Task}
+import mill.javalib.{Dep, DepSyntax, JavaModule, PublishModule}
+import mill.util.Jvm
+import upickle.default.ReadWriter.join
+
+import java.net.URLClassLoader
+
+@mill.api.experimental
+trait QuarkusModule extends JavaModule {
+
+  /**
+   * The version of the quarkus platform (e.g. 3.31.2). Used for
+   * setting the `io.quarkus.platform:quarkus-bom` version
+   * It's used for creating the quarkus Application Model and bootstrapping this Quarkus module
+   *
+   * For the latest version check [[https://quarkus.io/]]
+   */
+  def quarkusPlatformVersion: T[String]
+
+  /**
+   * If this is a [[PublishModule]] then group id is derived from [[PublishModule.pomSettings]]
+   * otherwise it needs to be setup by the user.
+   */
+  def artifactGroupId: T[String] = this match {
+    case m: PublishModule =>
+      Task {
+        m.pomSettings().organization
+      }
+    case _ =>
+      Task {
+        Task.fail(
+          "The moduleGroupId is not set. Please override the moduleGroupId so quarkus can generate the application model for this module"
+        )
+      }
+  }
+
+  /**
+   * If this is a [[PublishModule]] then artifact version is derived from [[PublishModule.publishVersion]]
+   * otherwise it needs to be setup by the user.
+   *
+   * It's used for creating the quarkus Application Model and bootstrapping this Quarkus module
+   */
+  def artifactVersion: T[String] = this match {
+    case m: PublishModule =>
+      Task {
+        m.publishVersion()
+      }
+    case _ =>
+      Task {
+        Task.fail(
+          "The artifactVersion is not set. Please override the artifactVersion so quarkus can generate the application model for this module"
+        )
+      }
+  }
+
+  /**
+   * The artifact id used to Quarkus bootstrap this module.
+   * It needs to be set and a non-empty String for the Quarkus application model serialization to work!
+   */
+  def artifactId: T[String] = Task {
+    Option(super.artifactId()).filterNot(_.isEmpty)
+      .getOrElse(Task.fail(
+        "The artifactVersion is not set. Please override the artifactVersion so quarkus can generate the application model for this module"
+      ))
+  }
+
+  override def bomMvnDeps: Task.Simple[Seq[Dep]] = super.bomMvnDeps() ++ Seq(
+    mvn"io.quarkus.platform:quarkus-bom:${quarkusPlatformVersion()}"
+  )
+
+  /**
+   * Dependencies for the Quarkus Bootstrap process
+   * needed for [[quarkusApplicationModelWorker]]
+   */
+  def quarkusBootstrapDeps: T[Seq[Dep]] = Task {
+    Seq(
+      mvn"io.quarkus:quarkus-bootstrap-core",
+      mvn"io.quarkus:quarkus-bootstrap-app-model",
+      mvn"io.quarkus:quarkus-bootstrap-maven-resolver",
+      mvn"io.quarkus:quarkus-core-deployment"
+    )
+  }
+
+  /**
+   * The native image for building this Quarkus module.
+   * Can be mandrel, graalvm or a full image path.
+   *
+   * For more info see [[https://quarkus.io/guides/building-native-image#background]]
+   */
+  def quarkusNativeImage: T[String] = Task {
+    "mandrel"
+  }
+
+  /**
+   * Quarkus builds its own run classpath and manages it
+   * via the launcher (quarkus-run.jar) which handles
+   * running the application.
+   */
+  override def runClasspath: T[Seq[PathRef]] = Task {
+    Seq(quarkusRunJar())
+  }
+
+  /**
+   * Quarkus builds its own run classpath and manages it
+   * via the launcher (quarkus-run.jar) which handles
+   * and doesn't need a main class. However, for mill run to work
+   * we need to put the Quarkus entrypoint here which is `io.quarkus.bootstrap.runner.QuarkusEntryPoint`
+   */
+  override def finalMainClass: T[String] = "io.quarkus.bootstrap.runner.QuarkusEntryPoint"
+
+  def quarkusApplicationModelWorkerClassloader: Task.Worker[URLClassLoader] = Task.Worker {
+
+    val classpath = defaultResolver().classpath(
+      quarkusBootstrapDeps() ++ Seq(Dep.millProjectModule("mill-libs-javalib-quarkus-worker")),
+      boms = allBomDeps()
+    )
+
+    Jvm.createClassLoader(classpath.map(_.path), parent = getClass.getClassLoader)
+  }
+
+  /**
+   * The worker which provides the Quarkus Bootstrap process for creating
+   * the quarkus Application Model and building the Application itself.
+   *
+   * This model is used by the QuarkusBootstrap to derive the dependencies
+   * and the build steps (e.g. packaging a jar or a native app), checking
+   * which dependencies are runtime, which are deployable etc.
+   *
+   * See also [[quarkusDependencies]] and [[quarkusApplicationModelWorker]] for following the
+   * full implementation steps.
+   */
+  def quarkusApplicationModelWorker: Task.Worker[ApplicationModelWorker] = Task.Worker {
+    quarkusApplicationModelWorkerClassloader().loadClass(
+      "mill.javalib.quarkus.ApplicationModelWorkerImpl"
+    )
+      .getDeclaredConstructor()
+      .newInstance()
+      .asInstanceOf[ApplicationModelWorker]
+  }
+
+  /**
+   * These are the application dependencies that Quarkus needs to know about
+   * for bootstrapping. They generally are in 3 categories:
+   * 1. Runtime applications
+   * 2. Deployment applications (that are also runtime)
+   * 3. Compile applications
+   * In addition, other helpful flags, help the [[ApplicationModelWorker]] to
+   * flag these dependencies correctly, such as marking top level artifacts (i.e. direct dependencies).
+   *
+   * This mechanism is not fully implemneted yet, and only works for a single module.
+   *
+   * TODO send multiple modules to the Quarkus Bootstrap process using the moduleDeps
+   */
+  def quarkusDependencies: Task[Seq[ApplicationModelWorker.Dependency]] = Task.Anon {
+    val depRuntime = coursierDependencyTask().withVariantSelector(
+      ConfigurationBased(coursier.core.Configuration.runtime)
+    )
+
+    val depCompile = coursierDependencyTask().withVariantSelector(
+      ConfigurationBased(coursier.core.Configuration.compile)
+    )
+
+    val runtimeDeps =
+      millResolver().artifacts(Seq(mill.javalib.BoundDep(depRuntime, force = false)))
+
+    def qualifier(d: coursier.core.Dependency) =
+      s"${d.module.organization.value}:${d.module.name.value}"
+
+    def wQualifier(d: ApplicationModelWorker.Dependency) =
+      s"${d.groupId}:${d.artifactId}"
+
+    def isDirectDep(d: coursier.core.Module): Boolean =
+      mvnDeps().exists(dep => dep.dep.module == d)
+
+    val compileDeps =
+      millResolver().artifacts(Seq(mill.javalib.BoundDep(depCompile, force = false)))
+
+    val runtimeDepSet = runtimeDeps.detailedArtifacts0.map(da => qualifier(da._1)).toSet
+
+    val quarkusPrecomputedRuntimeDeps = runtimeDeps.detailedArtifacts0.map {
+      case (dependency, _, _, file) =>
+        ApplicationModelWorker.Dependency(
+          groupId = dependency.module.organization.value,
+          artifactId = dependency.module.name.value,
+          version = dependency.versionConstraint.asString,
+          resolvedPath = os.Path(file),
+          isRuntime = true,
+          isDeployment = false,
+          isTopLevelArtifact = isDirectDep(dependency.module),
+          hasExtension = false
+        )
+    }
+
+    val depsWithExtensions = quarkusApplicationModelWorker().quarkusDeploymentDependencies(
+      quarkusPrecomputedRuntimeDeps
+    )
+
+    val extensionDepsSet = depsWithExtensions.map(wQualifier).toSet
+
+    val deploymentMvnDeps = depsWithExtensions.map(d =>
+      mvn"${d.groupId}:${d.artifactId}-deployment:${d.version}"
+    )
+
+    val deploymentDeps = millResolver().artifacts(
+      deploymentMvnDeps
+    )
+
+    val deploymentDepsSet = deploymentDeps.detailedArtifacts0.map(da => qualifier(da._1)).toSet
+
+    val quarkusDeploymentDeps = deploymentDeps.detailedArtifacts0.map {
+      case (dependency, _, _, file) =>
+        ApplicationModelWorker.Dependency(
+          groupId = dependency.module.organization.value,
+          artifactId = dependency.module.name.value,
+          version = dependency.versionConstraint.asString,
+          resolvedPath = os.Path(file),
+          isRuntime = runtimeDepSet.contains(qualifier(dependency)),
+          isDeployment = true,
+          isTopLevelArtifact = isDirectDep(dependency.module),
+          hasExtension = extensionDepsSet.contains(qualifier(dependency))
+        )
+    }
+
+    val quarkusRuntimeDeps = quarkusPrecomputedRuntimeDeps.filterNot(d =>
+      deploymentDepsSet.contains(wQualifier(d))
+    )
+
+    val quarkusCompileDeps =
+      compileDeps.detailedArtifacts0.filterNot {
+        da =>
+          val q = qualifier(da._1)
+          runtimeDepSet.contains(q) || deploymentDepsSet.contains(q)
+      }.map {
+        case (dependency, _, _, file) =>
+          ApplicationModelWorker.Dependency(
+            groupId = dependency.module.organization.value,
+            artifactId = dependency.module.name.value,
+            version = dependency.versionConstraint.asString,
+            resolvedPath = os.Path(file),
+            isRuntime = false,
+            isDeployment = false,
+            isTopLevelArtifact = isDirectDep(dependency.module),
+            hasExtension = false
+          )
+      }
+
+    quarkusRuntimeDeps ++ quarkusCompileDeps ++ quarkusDeploymentDeps
+  }
+
+  /**
+   * The quarkus Application Model requires a build file. While
+   * in Mill, we don't let Quarkus resolve any dependencies (quarkus itself
+   * does not have mill support to understand what's going on) this exists
+   * to keep the Quarkus Application model serialization from complaining.
+   *
+   * For now, we pass a dummy file.
+   */
+  def quarkusMillBuildFile: Task.Simple[PathRef] = Task {
+    val dummyFile = Task.dest / "dummy_build_file"
+    os.write(dummyFile, "dummy")
+    PathRef(dummyFile)
+  }
+
+  def quarkusSerializedAppModel: T[PathRef] = Task {
+    val modelPath = quarkusApplicationModelWorker().quarkusGenerateApplicationModel(
+      ApplicationModelWorker.AppModel(
+        projectRoot = moduleDir,
+        buildDir = compile().classes.path,
+        buildFile = quarkusMillBuildFile().path,
+        quarkusVersion = quarkusPlatformVersion(),
+        groupId = artifactGroupId(),
+        artifactId = artifactId(),
+        version = artifactVersion(),
+        sourcesDir = sources().head.path, // TODO support multiple
+        resourcesDir = resources().head.path,
+        compiledPath = compile().classes.path,
+        compiledResources = compileResources().head.path, // TODO this is wrong, adjust later,
+        boms = bomMvnDeps().map(_.formatted),
+        dependencies = quarkusDependencies(),
+        nativeImage = quarkusNativeImage()
+      ),
+      Task.dest
+    )
+    PathRef(modelPath)
+
+  }
+
+  /**
+   * The quarkus-run.jar which is a standalone fast jar
+   * created by QuarkusBootstrap using the generated ApplicationModel
+   * generated from the [[quarkusSerializedAppModel]]
+   * @return the path of the quarkus-run.jar
+   */
+  def quarkusRunJar: T[PathRef] = Task {
+    val dest = Task.dest / "quarkus"
+    os.makeDir.all(dest)
+    val jarPath = quarkusApplicationModelWorker().quarkusBootstrapApplication(
+      quarkusSerializedAppModel().path,
+      dest / "quarkus-run.jar", // TODO use quarkus utility function
+      jar().path
+    )
+
+    PathRef(jarPath)
+  }
+
+}
